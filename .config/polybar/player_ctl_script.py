cd22285ca9ef09ce269d9a98a377bae824bda82d
LeafyEmerald <private.wafflevideos@gmail.com>
2021-07-23 20:08:18 +0300

    added changed files

--- .config/polybar/player_ctl_script.py
+++ .config/polybar/player_ctl_script.py
@@ -7,28 +7,66 @@ from gi.repository import GLib
 #loop.run()
 bus = dbus.SessionBus()
 
+mprisMediaPlayer = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
+mpris = dbus.Interface(mprisMediaPlayer, 'org.mpris.MediaPlayer2.Player')
 
+def playSong(self):
+    mpris.Play()
 
+def pauseSong(self):
+    mpris.Pause()
+
+def nextSong(self):
+    mpris.Next()
+
+def prevSong(self):
+    mpris.Previous()
+
+#mpris.Play()
+
+def getSongArt():
+    metadata = getSongMetadata()
+    art = "https://i.scdn.co/image/"+metadata['mpris:artUrl'].split("/")[4]
+    #print(art)
+    return art
+
+def getTitle():
+    metadata = getSongMetadata()
+    title = str(metadata['xesam:title'])
+    return title
+
+def getArtist():
+    metadata = getSongMetadata()
+    artist = str(metadata['xesam:artist'][0])
+    return artist
+
+def getLen():
+    metadata = getSongMetadata()
+    len = int(metadata['mpris:length'])
+    return len
+
+def getAlbum():
+    metadata = getSongMetadata()
+    album = str(metadata['xesam:album'])
+    return album
 
 def getSongMetadata():
-    for service in bus.list_names():
-        if service.startswith('org.mpris.MediaPlayer2.'):
-            player = dbus.SessionBus().get_object(service, '/org/mpris/MediaPlayer2')
+    #for service in bus.list_names():
+        #if service.startswith('org.mpris.MediaPlayer2.'):
+            #player = dbus.SessionBus().get_object(, '/org/mpris/MediaPlayer2')
 
-            status=player.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus', dbus_interface='org.freedesktop.DBus.Properties')
-            metadata = player.Get('org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interface='org.freedesktop.DBus.Properties')
+            #status=mpris.Get('org.mpris.MediaPlayer2.spotify', 'PlaybackStatus', dbus_interface='org.freedesktop.DBus.Properties')
+            metadata = mpris.Get('org.mpris.MediaPlayer2.Player', 'Metadata', dbus_interface='org.freedesktop.DBus.Properties')
         
             #for key in metadata:
-            title = str(metadata['xesam:title'])
-            len = int(metadata['mpris:length'])
-            artist = str(metadata['xesam:artist'][0])
-            art = "https://i.scdn.co/image/"+metadata['mpris:artUrl'].split("/")[4]
-            album = str(metadata['xesam:album'])
-
-            metadata_list = title, art, len, artist, album
-            # print(metadata_list)
-            
-            return metadata_list
+            #title = str(metadata['xesam:title'])
+            #len = int(metadata['mpris:length'])
+            #artist = str(metadata['xesam:artist'][0])
+            #art = "https://i.scdn.co/image/"+metadata['mpris:artUrl'].split("/")[4]
+            #album = str(metadata['xesam:album'])            
+            return metadata
 
 if __name__ == '__main__':
+    #playSong()
     getSongMetadata()
+    print(getArtist())
